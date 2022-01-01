50d96d6cb03d97410004aa3456fbcca617b74886
michael-lugashi <michael.lugashi@gmail.com>
2022-01-01 15:31:06 +0200

    downloaded project dependencies

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = 8080;
+
+io.on('connection', (socket) => {
+ socket.on('message', ({ name, message }) => {
+  io.emit('messageBack', { name, message });
+ });
+
+ socket.on('disconnect', () => {
+  io.emit('messageBack', { message: 'disconnected' });
+ });
+});
+
+http.listen(port, () => {
+ console.log(`listening on port ${port}`);
+});
