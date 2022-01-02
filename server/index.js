3b38a0e9e6c3be4f53a529b4d6a54617cc182a68
michael-lugashi <michael.lugashi@gmail.com>
2022-01-03 01:13:03 +0200

    heroku deploy work

--- server/index.js
+++ server/index.js
@@ -20,6 +20,7 @@ app.get('/', function (req, res) {
 const io = require('socket.io')(http, {
  cors: {
   origin: [
+   '/',
    'http://localhost:8080/',
    'https://minesweeper-pvp.herokuapp.com/',
    'ws://localhost:8080/',
